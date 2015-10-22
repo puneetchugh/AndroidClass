58a551ded0647a6b73c887a872e95822885f1d6e
Puneet <puneet.chugh@nyu.edu>
2015-10-21 21:46:10 -0400

    Contains first assignment

---
+++ ETA_CS9033_chughpuneet/app/src/main/java/com/nyu/cs9033/eta/controllers/CreateTripActivity.java
@@ -0,0 +1,180 @@
+package com.nyu.cs9033.eta.controllers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.nyu.cs9033.eta.R;
+import com.nyu.cs9033.eta.models.Person;
+import com.nyu.cs9033.eta.models.Trip;
+
+public class CreateTripActivity extends Activity {
+	
+	private static final String TAG = "CreateTripActivity";
+	private static final String MAIN_TAG = "MainActivity";
+	private static final String TRIP_DATA = "TripData";
+	private static final int RESULT_CODE = 2;
+	private Intent intent;
+	private CheckBox puneet;
+	private CheckBox hannah;
+	private CheckBox pranay;
+	private CheckBox chenxi;
+	private CheckBox sheryar;
+	private EditText tripDate;
+	private EditText tripTime;
+	private EditText tripLocation;
+	private Button createButton;
+	private Button cancelButton;
+	private EditText tripName;
+
+	Person personPuneet, personHannah, personPranay, personChenxi, personSheryar;
+	//private Object View;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// TODO - fill in here
+		setContentView(R.layout.create_trip);
+
+		tripName = (EditText)findViewById(R.id.type_name_id);
+		puneet = (CheckBox) findViewById(R.id.puneet_id);
+		hannah = (CheckBox) findViewById(R.id.hannah_id);
+		pranay = (CheckBox) findViewById(R.id.pranay_id);
+		chenxi = (CheckBox) findViewById(R.id.chenxi_id);
+		sheryar = (CheckBox) findViewById(R.id.sheryar_id);
+
+		tripDate = (EditText) findViewById(R.id.date_id);
+		tripTime = (EditText) findViewById(R.id.time_id);
+		tripLocation = (EditText) findViewById(R.id.location_id);
+		tripName = (EditText) findViewById(R.id.type_name_id);
+		createButton = (Button) findViewById(R.id.create_button_id);
+		createButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+
+
+				Trip trip = createTrip();
+
+
+				saveTrip(trip);
+
+
+			}
+
+		});
+
+	}
+
+	/**
+	 * This method should be used to
+	 * instantiate a Trip model object.
+	 * 
+	 * @return The Trip as represented
+	 * by the View.
+	 */
+	public Trip createTrip() {
+
+
+		// TODO - fill in here
+
+		String location = tripLocation.getText().toString();
+		String date = tripDate.getText().toString();
+		String time = tripTime.getText().toString();
+		String name = tripName.getText().toString();
+		if(puneet.isChecked()){
+			personPuneet = new Person("Puneet");
+		}
+		else{
+			personPuneet = new Person("");
+		}
+
+		if(hannah.isChecked()){
+			personHannah = new Person("Hannah");
+		}
+		else{
+			personHannah = new Person("");
+		}
+
+		if(pranay.isChecked()){
+			personPranay = new Person("Pranay");
+		}
+		else{
+			personPranay = new Person("");
+		}
+
+		if(chenxi.isChecked()){
+			personChenxi = new Person("Chenxi");
+		}
+		else{
+			personChenxi = new Person("");
+		}
+
+		if(sheryar.isChecked()){
+			personSheryar = new Person("Sheryar");
+		}
+		else{
+			personSheryar = new Person("");
+		}
+
+
+
+
+		Trip trip = new Trip(location,date,time,personPuneet.getName(),personHannah.getName(),personPranay.getName(),personChenxi.getName(),personSheryar.getName(),name );
+		return trip;
+		//return null;
+	}
+
+	/**
+	 * For HW2 you should treat this method as a 
+	 * way of sending the Trip data back to the
+	 * main Activity.
+	 * 
+	 * Note: If you call finish() here the Activity 
+	 * will end and pass an Intent back to the
+	 * previous Activity using setResult().
+	 * 
+	 * @return whether the Trip was successfully 
+	 * saved.
+	 */
+	public boolean saveTrip(Trip trip) {
+	
+		// TODO - fill in here
+
+		Intent intent = new Intent(CreateTripActivity.this, MainActivity.class);
+		//Bundle newBundle = new Bundle();
+		//newBundle.putParcelable(TRIP_DATA,trip);
+		//intent.putExtras(newBundle);
+		intent.putExtra(TRIP_DATA,trip);
+		setResult(RESULT_CODE, intent);
+		finish();
+
+		return true;
+		//return false;
+	}
+
+	/**
+	 * This method should be used when a
+	 * user wants to cancel the creation of
+	 * a Trip.
+	 * 
+	 * Note: You most likely want to call this
+	 * if your activity dies during the process
+	 * of a trip creation or if a cancel/back
+	 * button event occurs. Should return to
+	 * the previous activity without a result
+	 * using finish() and setResult().
+	 */
+	public void cancelTrip() {
+
+		finish();
+		// TODO - fill in here
+	}
+
+
+}
---
+++ ETA_CS9033_chughpuneet/app/src/main/java/com/nyu/cs9033/eta/models/Trip.java
@@ -0,0 +1,157 @@
+package com.nyu.cs9033.eta.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Trip implements Parcelable {
+	
+	// Member fields should exist here, what else do you need for a trip?
+	// Please add additional fields
+	private String location;
+	private String time;
+	private String date;
+	private String firstPerson;
+	private String secondPerson;
+	private String thirdPerson;
+	private String fourthPerson;
+	private String fifthPerson;
+	private String tripName;
+	
+	/**
+	 * Parcelable creator. Do not modify this function.
+	 */
+	public static final Parcelable.Creator<Trip> CREATOR = new Parcelable.Creator<Trip>() {
+		public Trip createFromParcel(Parcel p) {
+			return new Trip(p);
+		}
+
+		public Trip[] newArray(int size) {
+			return new Trip[size];
+		}
+	};
+	
+	/**
+	 * Create a Trip model object from a Parcel. This
+	 * function is called via the Parcelable creator.
+	 * 
+	 * @param p The Parcel used to populate the
+	 * Model fields.
+	 */
+	public Trip(Parcel p) {
+		
+		// TODO - fill in here
+		//String[] data = new String[8];
+		//p.readStringArray(data);
+		this.location = p.readString();
+		this.date = p.readString();
+		this.time = p.readString();
+		this.firstPerson = p.readString();
+		this.secondPerson = p.readString();
+		this.thirdPerson = p.readString();
+		this.fourthPerson = p.readString();
+		this.fifthPerson = p.readString();
+		this.tripName = p.readString();
+	}
+	
+	/**
+	 * Create a Trip model object from arguments
+	 * 
+	 * @param name  Add arbitrary number of arguments to
+	 * instantiate Trip class based on member variables.
+	 *
+	 * @param
+	 */
+	public Trip(String location, String date, String time, String firstPerson, String secondPerson, String thirdPerson, String fourthPerson, String fifthPerson, String tripName) {
+		
+		// TODO - fill in here, please note you must have more arguments here
+		this.location = location;
+		this.date = date;
+		this.time = time;
+		this.firstPerson = firstPerson;
+		this.secondPerson = secondPerson;
+		this.thirdPerson = thirdPerson;
+		this.fourthPerson = fourthPerson;
+		this.fifthPerson = fifthPerson;
+		this.tripName = tripName;
+	}
+
+	/**
+	 * Serialize Trip object by using writeToParcel. 
+	 * This function is automatically called by the
+	 * system when the object is serialized.
+	 * 
+	 * @param dest Parcel object that gets written on 
+	 * serialization. Use functions to write out the
+	 * object stored via your member variables. 
+	 * 
+	 * @param flags Additional flags about how the object 
+	 * should be written. May be 0 or PARCELABLE_WRITE_RETURN_VALUE.
+	 * In our case, you should be just passing 0.
+	 */
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		
+		// TODO - fill in here
+//		dest.writeStringArray(new String[] {
+				dest.writeString(this.location);
+				dest.writeString(this.date);
+				dest.writeString(this.time);
+				dest.writeString(this.firstPerson);
+				dest.writeString(this.secondPerson);
+				dest.writeString(this.thirdPerson);
+				dest.writeString(this.fourthPerson);
+				dest.writeString(this.fifthPerson);
+				dest.writeString(this.tripName);
+//		});
+	}
+	
+	/**
+	 * Feel free to add additional functions as necessary below.
+	 */
+
+	public String getLocation(){
+		return location;
+	}
+
+	public String getTime(){
+		return time;
+	}
+
+	public String getDate(){
+		return date;
+	}
+
+	public String getFirstPerson(){
+		return firstPerson;
+	}
+
+	public String getSecondPerson(){
+		return secondPerson;
+	}
+
+	public String getThirdPerson(){
+		return thirdPerson;
+	}
+
+	public String getFourthPerson(){
+		return fourthPerson;
+	}
+
+	public String getFifthPerson(){
+		return fifthPerson;
+	}
+
+	public String getTripName(){
+		return tripName;
+	}
+	/**
+	 * Do not implement
+	 */
+	@Override
+	public int describeContents() {
+		// Do not implement!
+		return 0;
+	}
+
+
+}

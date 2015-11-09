b2b3bc96ea160258f26119d6971d732998a96109
Puneet <puneet.chugh@nyu.edu>
2015-11-09 01:34:44 -0500

    Starting listView - committing for easy recovery

--- ETA_CS9033_chughpuneet/app/src/main/java/com/nyu/cs9033/eta/controllers/CustomAdapter.java
+++ ETA_CS9033_chughpuneet/app/src/main/java/com/nyu/cs9033/eta/controllers/CustomAdapter.java
@@ -1,6 +1,147 @@
 package com.nyu.cs9033.eta.controllers;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nyu.cs9033.eta.models.Trip;
+
+import java.util.ArrayList;
+
 /**
  * Created by puneet on 11/8/15.
  */
+public class CustomAdapter extends BaseAdapter implements View.OnClickListener {
 
+    /*********** Declare Used Variables *********/
+    private Activity activity;
+    private ArrayList<Trip> trips;
+    private static LayoutInflater inflater=null;
+    public Resources res;
+    Trip tempValues=null;
+    int i=0;
+
+    /*************  CustomAdapter Constructor *****************/
+    public CustomAdapter(Activity activity, ArrayList<Trip> trips,Resources res) {
+
+        /********** Take passed values **********/
+        this.activity = activity;
+        this.trips = trips;
+        this.res = res;
+
+        /***********  Layout inflator to call external xml layout () ***********/
+        inflater = ( LayoutInflater )activity.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    /******** What is the size of Passed Arraylist Size ************/
+    public int getCount() {
+
+        if(trips.size()<=0)
+            return 1;
+        return trips.size();
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /********* Create a holder Class to contain inflated xml file elements *********/
+    public static class ViewHolder{
+
+        public TextView text;
+        public TextView text1;
+        public TextView textWide;
+        public ImageView image;
+
+    }
+
+    /****** Depends upon data size called for each row , Create each ListView row *****/
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View vi = convertView;
+        ViewHolder holder;
+
+        if(convertView==null){
+
+            /****** Inflate tabitem.xml file for each row ( Defined below ) *******/
+            vi = inflater.inflate(R.layout.tabitem, null);
+
+            /****** View Holder Object to contain tabitem.xml file elements ******/
+
+            holder = new ViewHolder();
+            holder.text = (TextView) vi.findViewById(R.id.text);
+            holder.text1=(TextView)vi.findViewById(R.id.text1);
+            holder.image=(ImageView)vi.findViewById(R.id.image);
+
+            /************  Set holder with LayoutInflater ************/
+            vi.setTag( holder );
+        }
+        else
+            holder=(ViewHolder)vi.getTag();
+
+        if(trips.size()<=0)
+        {
+            holder.text.setText("No Data");
+
+        }
+        else
+        {
+            /***** Get each Model object from Arraylist ********/
+            tempValues=null;
+            tempValues = ( Trip ) trips.get( position );
+
+            /************  Set Model values in Holder elements ***********/
+
+            holder.text.setText( tempValues.getCompanyName() );
+            holder.text1.setText( tempValues.getUrl() );
+            holder.image.setImageResource(
+                    res.getIdentifier(
+                            "com.androidexample.customlistview:drawable/"+tempValues.getImage()
+                            ,null,null));
+
+            /******** Set Item Click Listner for LayoutInflater for each row *******/
+
+            vi.setOnClickListener(new OnItemClickListener( position ));
+        }
+        return vi;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.v("CustomAdapter", "=====Row button clicked=====");
+    }
+
+    /********* Called when Item click in ListView ************/
+    private class OnItemClickListener  implements View.OnClickListener {
+
+        private int mPosition;
+
+        OnItemClickListener(int position){
+            mPosition = position;
+        }
+
+        @Override
+        public void onClick(View arg0) {
+
+
+            CustomListViewAndroidExample sct = (CustomListViewAndroidExample)activity;
+
+            /****  Call  onItemClick Method inside CustomListViewAndroidExample Class ( See Below )****/
+
+            sct.onItemClick(mPosition);
+        }
+    }
+}
